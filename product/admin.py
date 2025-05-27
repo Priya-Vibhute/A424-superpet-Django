c2b972731db3fd00bd024dd2fd252f328ef66369
Priya-Vibhute <priyanka.vibhute@itvedant.com>
2025-05-27 14:59:36 +0530

    27 May 2025 2

--- product/admin.py
+++ product/admin.py
@@ -2,4 +2,8 @@ from django.contrib import admin
 from .models import Product
 # Register your models here.
 
-admin.site.register(Product)
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display=['id','product_name','product_description']
+
+admin.site.register(Product,ProductAdmin)
--- product/models.py
+++ product/models.py
@@ -5,4 +5,7 @@ class Product(models.Model):
     product_name=models.CharField(max_length=70)
     product_description=models.TextField()
     product_price=models.PositiveIntegerField()
-    
+
+    def __str__(self):
+        return self.product_name
+
